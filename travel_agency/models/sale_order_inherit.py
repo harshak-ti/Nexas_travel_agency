b1c03b04e1a3b0ef4c5822f2a84b91b49f06a808
harshak-ti <harshak.saini@targetintegration.com>
2024-09-17 17:14:11 +0530

    first-commit

--- travel_agency/models/sale_order_inherit.py
+++ travel_agency/models/sale_order_inherit.py
@@ -2,9 +2,13 @@ from odoo import fields,models,api
 import logging
 _l=logging.getLogger(__name__)
 class SaleOrder(models.Model):
+
     _inherit='sale.order'
 
     status=fields.Selection([('pending','Pending'),('confirmed','Confirmed'),('cancelled','Cancelled')],string="Status",default='pending')
+
+
+    name=fields.Char(readonly=False)
 
     def action_confirm(self):
         for rec in self:
@@ -24,10 +28,42 @@ class SaleOrder(models.Model):
             'target':'new'
         }
         for rec in self:
-            self.status='cancelled'
+            self.button_click_method()
 
     def confirm_booking(self):
         self.ensure_one()
         self.status = 'confirmed'
         return True
     
+
+    def button_click_method(self):
+        if self.env.context.get('user_clicked',False):
+            self.status='confirmed'
+            self.func1()
+        else:
+            self.status='cancelled'
+
+
+    def func1(self):
+        self=self.with_context(a=20,b=10)
+        _l.info(f"---------------> \n Value of a = {self.env.context.get('a')}\n Value of b = {self.env.context.get('b')}")
+
+        _l.info(f'-------->func1 before context is {self._context}')
+
+        self=self.func2()
+
+        _l.info(f'-------->func1 after context is {self._context}')
+
+        _l.info(f"---------------> \n Value of a = {self.env.context.get('a')}\n Value of b = {self.env.context.get('b')}")
+    
+    def func2(self):
+        _l.info(f"---------------> \n Func2 Value of a = {self.env.context.get('a')}\n Value of b = {self.env.context.get('b')}")
+        _l.info(f'-------->func2 before context is {self._context}')
+        # self=self.with_context(a=300,b=200)
+        context=dict(self.env.context)
+        context.pop('b',None)
+        self=self.with_context(context)
+        _l.info(f'-------->func2 after context is {self._context}')
+        return self
+
+
